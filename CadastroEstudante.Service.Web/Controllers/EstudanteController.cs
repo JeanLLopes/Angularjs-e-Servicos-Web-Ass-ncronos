c0b500cc8448120b8aabcc22c587bd63285371f9
JeanLLopes <jean.llopes@outlook.com>
2016-12-17 16:10:52 -0200

    Add Controller

--- CadastroEstudante.Service.Web/Controllers/EstudanteController.cs
+++ CadastroEstudante.Service.Web/Controllers/EstudanteController.cs
@@ -5,7 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-
+//IMPORTAMOS NOSSA CLASSE DE MODELOS PARA A CONTROLLER
+using CadastroEstudante.Service.Web.Areas.HelpPage.Models;
+using System.Data.SqlClient;
 
 namespace CadastroEstudante.Service.Web.Controllers
 {
@@ -18,8 +20,41 @@ namespace CadastroEstudante.Service.Web.Controllers
         /// <returns>MENSAGEM DE OK</returns>
         public HttpResponseMessage GetEstudante(int id)
         {
-            return Request.CreateResponse(HttpStatusCode.OK, 
-                                            "Estudante OK");
+            try
+            {
+                //AQUI  NOS CRIAMOS UM MODELO DE RETORNO DE DADOS
+                //PARA NOSSA API
+                var estudante = new EstudanteModel()
+                {
+                    Id = 1,
+                    Nome = "Jean Lopes",
+                    Email = "jean.llopes@outlook.com",
+                    Observacoes = "Nenhuma"
+                };
+
+                //AQUI NOS MONTAMOS O RETORNO DE DADOS PARA NOSSA API
+                //QUANDO TUDDO DER CERTO NOS IREMOS RETORNAR UM STATUS
+                //HTTP OK, E OS DADOS DO ESTUDANTE
+                return Request.CreateResponse(HttpStatusCode.OK, estudante);
+            }
+            catch (SqlException)
+            {
+                return Request.CreateResponse(
+                    HttpStatusCode.InternalServerError,
+                    "Serviço Indisponivel");
+            }
+            catch (TimeoutException)
+            {
+                return Request.CreateResponse(
+                    HttpStatusCode.RequestTimeout,
+                    "Sua requisição deu TimeOut, Tente novamente mais tarde");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(
+                    HttpStatusCode.InternalServerError,
+                    "Serviço Indisponivel");
+            }
         }
     }
 }
